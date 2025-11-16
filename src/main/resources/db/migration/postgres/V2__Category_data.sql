efea49758178875de5a26bc06b02a33c095726ac
Ganeshan N <ganeshancse06@gmail.com>
2025-11-16 12:17:03 +0530

    postgres compactability added

---
+++ src/main/resources/db/migration/postgres/V2__Category_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO category (category, name) VALUES
+                                          ('TECHNOLOGY', 'Technology'),
+                                          ('BUSINESS',   'Business'),
+                                          ('FINANCE',    'Finance'),
+                                          ('POLITICS',   'Politics'),
+                                          ('SCIENCE',    'Science'),
+                                          ('OTHERS',     'Others');
